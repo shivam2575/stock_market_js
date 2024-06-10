697c85e7c77f27b16a03e4b902634158ea5c0e0a
shivam2575 <sk2575@gmail.com>
2024-06-10 22:39:38 +0530

    added code for adding details

--- script.js
+++ script.js
@@ -11,10 +11,37 @@ const stocks = [
   "DIS",
 ];
 const $stockContEle = $("#stock-cont");
+const $detailsParaEle = $("#details-para");
+
+function addDetails(url, name) {
+  $.getJSON(url, function (response) {
+    console.log(`calling for ${name}`);
+    let stockList = response.stocksProfileData[0];
+    console.log(stockList);
+    let stockDetails = stockList[name];
+    if (stockDetails) {
+      $detailsParaEle.text(stockDetails.summary);
+    }
+  });
+}
+
 $(document).ready(function () {
   stocks.forEach((stock) => {
-    const $stockNameEle = $("<h5></h5>").text(stock);
-    const $stockEle = $("<div></div>").append($stockNameEle);
+    const $stockNameEle = $("<button></button>").text(stock);
+    const $stockPriceEle = $("<span></span>").text(stock);
+    const $stockProfitEle = $("<span></span>").text(stock);
+    $stockNameEle.click(() => {
+      //   addChartInfo();
+      addDetails(
+        "https://stocks3.onrender.com/api/stocks/getstocksprofiledata",
+        stock
+      );
+    });
+    const $stockEle = $("<div></div>", { class: "list-div" }).append(
+      $stockNameEle,
+      $stockPriceEle,
+      $stockProfitEle
+    );
     $stockContEle.append($stockEle);
   });
 });
